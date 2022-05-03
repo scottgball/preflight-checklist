b501bc996fd4d4ebda04023b21393d77cb66868b
Scott Ball <scottgball@gmail.com>
2022-05-02 20:46:45 -0700

    Updated styles and added form validation

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ Vue.createApp({
   data() {
     return {
       aircraftList: [
-        {id: 0, name: 'Paramotor', checklistItems: [{id: 0, name: 'Check fuel', completed: false}, {id: 1, name: 'Inspect Propeller', completed: false},]},
+        {id: 0, name: 'Paramotor', checklistItems: [{id: 0, name: 'Check fuel', completed: false}, {id: 1, name: 'Inspect propeller', completed: false},]},
         {id: 1, name: 'Paraglider', checklistItems: [{id: 0, name: 'Clear lines', completed: false}]},
-        {id: 2, name: 'Cessna 172', checklistItems: [{id: 0, name: 'Check fuel', completed: false}]},
+        {id: 2, name: 'Cessna 172', checklistItems: [{id: 0, name: 'Check fuel', completed: false}, {id: 1, name: 'Inspect horizontal stabilizer', completed: false}, {id: 2, name: 'Inspect empennage', completed: false}]},
       ],
       newCraft: {id: null, name: '', checklistItems: []},
       newCraftFormDisplayed: false,
-      somethingSelected: false, //RETURN TO FALSE FOR DEFAULT!!!
-      selected: {}, //RETURN TO EMPTY OBJECT FOR DEFAULT!!!
+      somethingSelected: false,
+      selected: {},
       newItemFormDisplayed: false,
       newItem: {id: null, name: '', completed: false},
     }
@@ -18,17 +18,22 @@ Vue.createApp({
     selectCraft(craft) {
       this.selected = craft;
       this.somethingSelected = true;
+      this.newCraftFormDisplayed = false;
       this.newItemFormDisplayed = false;
     },
     toggleNewCraftForm() {
-      this.newCraftFormDisplayed = !this.newCraftFormDisplayed
+      this.newCraftFormDisplayed = !this.newCraftFormDisplayed;
     },
-    addNewCraft() {  //ADD FORM FOR NEW AIRCRAFT BUTTON
-      this.newCraft.id = this.aircraftList.length;
-      this.aircraftList.push(this.newCraft);
-      this.somethingSelected = true;
-      this.selected = this.newCraft;
-      this.newCraftFormDisplayed = false;
+      addNewCraft() {  //TODO: ADD FORM VALIDATION FOR EMPTY ENTRIES
+      if (this.newCraft.name.length) {
+        this.newCraft.id = this.aircraftList.length;
+        this.aircraftList.push(this.newCraft);
+        this.somethingSelected = true;
+        this.selected = this.newCraft;
+        this.newCraftFormDisplayed = false;
+        this.newItemFormDisplayed = false;
+        this.newCraft = {id: null, name: '', checklistItems: []};
+      } 
     },
     markCompleted(id) {
       this.selected.checklistItems[id].completed = !this.selected.checklistItems[id].completed;
@@ -37,12 +42,13 @@ Vue.createApp({
     toggleItemForm() {
       this.newItemFormDisplayed = !this.newItemFormDisplayed;
     },
-    addNewItem() {
-      this.newItem.id = this.selected.checklistItems.length;
-      this.selected.checklistItems.push(this.newItem);
-      this.newItem = {id: null, name: '', completed: false}
+    addNewItem() { //TODO: ADD FORM VALIDATION FOR EMPTY ENTRIES
+      if (this.newItem.name.length) {
+        this.newItem.id = this.selected.checklistItems.length;
+        this.selected.checklistItems.push(this.newItem);
+        this.newItem = {id: null, name: '', completed: false}
     }
-
+  }
   }
 })
 
